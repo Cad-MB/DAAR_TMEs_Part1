09ef290ce4e266747c7c472038f4490fc651d3cd
Abdelkader Boumessaoud <abdelkader.boumessaoud.pro@gmail.com>
2024-10-06 18:39:34 +0200

    correction samples

--- Backend/src/ndfa/export/NDFAGenerator.java
+++ Backend/src/ndfa/export/NDFAGenerator.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 /**
  * The {@code NDFAGenerator} class reads regular expressions from a CSV-like file,
